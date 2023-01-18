29601801ca89f6183c5881f2c23d0f2b1dc8b60a
Thiago AS <thiago-pin@gotmail.com>
2023-01-18 11:22:10 -0300

    style: add main height media screen

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,6 +4,15 @@ export const Container = styled.main`
   display: flex;
   height: 70vh;
   width: 100%;
+  min-height: 450px;
 
   border: 2px solid var(--lightblue-border);
+
+  @media screen and (max-width: 1024px) {
+    height: 45vh;
+  }
+
+  @media screen and (min-width: 1440px) {
+    height: 45vh;
+  }
 `;
